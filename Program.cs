178cf71f292cba34e5c2de34bd3e740c223f65e7
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2026-02-08 13:12:21 +0100

    err

--- Program.cs
+++ Program.cs
@@ -2,6 +2,21 @@
 {
     internal class Program
     {
+        static int es7(string parola, char lettera)
+        {
+            char[] lettere = parola.ToCharArray();
+
+            int l = 0;
+
+            for (int i = 0; i < lettere.Length; i++)
+            {
+                if (lettere[i] == lettera)
+                {
+                    l++;
+                }
+            }
+            return l;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Dimmi una parola");
@@ -10,19 +25,7 @@
             Console.WriteLine("Dimmi una lettera nella parola");
             char lettera = Convert.ToChar(Console.ReadLine());
 
-            char[] lettere = parola.ToCharArray();
-
-            int l = 0;
-
-            for(int i = 0; i < lettere.Length; i++)
-            {
-                if (lettere[i] == lettera)
-                {
-                    l++;
-                }
-            }
-
-            Console.WriteLine(l);
+            Console.WriteLine(es7(parola, lettera));
         }
     }
 }
