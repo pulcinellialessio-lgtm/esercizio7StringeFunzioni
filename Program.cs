2b7923c719edfa90e4671f4b5257f59b920f8401
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2026-02-08 12:38:24 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace esercizio7StringeFunzioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dimmi una parola");
+            string parola = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Dimmi una lettera nella parola");
+            char lettera = Convert.ToChar(Console.ReadLine());
+
+            char[] lettere = parola.ToCharArray();
+
+            int l = 0;
+
+            for(int i = 0; i < lettere.Length; i++)
+            {
+                if (lettere[i] == lettera)
+                {
+                    l++;
+                }
+            }
+
+            Console.WriteLine(l);
+        }
+    }
+}
